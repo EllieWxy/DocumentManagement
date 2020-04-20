4ede163443613529ea51096bb64f4e8a5196e6b4
jerrykingxyz <jerrykingxyz@gmail.com>
2020-04-20 22:43:02 +0800

    feat: vditor

--- webapp/src/components/MDEditor/index.tsx
+++ webapp/src/components/MDEditor/index.tsx
@@ -1,15 +1,23 @@
-// import * as React from "react";
-// import './index.css'
-// import Vditor from 'vditor'
-// // import "vditor/src/dist/index.css"
-//
-// export default class MDEditor extends React.Component{
-//
-//     render(){
-//         console.log(Vditor)
-//         // const vditor = new Vditor('editor')
-//         return <div>
-//         </div>
-//     }
-// }
+import * as React from "react";
+import "./index.css";
+import Vditor from "vditor";
 
+import "vditor/dist/index.css";
+
+export default class MDEditor extends React.Component<any, any> {
+  rootRef = React.createRef<HTMLDivElement>();
+  vditor: Vditor | null = null;
+
+  componentDidMount() {
+    this.vditor = new Vditor(this.rootRef.current, {
+      cache: { enable: false },
+      toolbarConfig: {
+        hide: true
+      }
+    });
+  }
+
+  render() {
+    return <div ref={this.rootRef} />;
+  }
+}
