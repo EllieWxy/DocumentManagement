007c1ab1ed2f0567deb0cdb9c9181e75153487f2
wangxiaoyi <wangxiaoyi999@foxmail.com>
2020-04-22 18:34:51 +0800

    feat : 添加后台文件树算法

--- modules/file.js
+++ modules/file.js
@@ -2,29 +2,41 @@ const fileModel = require('../models').File
 const countersModel = require('../models').Couters
 const JSONError = require('../utils/JSONError')
 
-exports.addFile = function (fid,cid,title,content,father,key) {
+exports.addFile = function (fid, cid, title, content, father, key) {
   fileModel.create({
-    fid:fid,
-    cid:cid,
-    title:title,
-    content:content,
-    father:father,
-    key:key
+    fid: fid,
+    cid: cid,
+    title: title,
+    content: content,
+    father: father,
+    key: key
   })
 }
 
-exports.fatherAddChild = function(father,fid,title){
-  return fileModel.findOneAndUpdate({fid:father},{"$push":{"childNodes":{fid:fid,title:title}}});
+exports.fatherAddChild = function (father, fid, title) {
+  return fileModel.findOneAndUpdate({fid: father}, {"$push": {"childNodes": {fid: fid, title: title}}});
 }
 
-exports.ifHaveChild = function(fid){
-  return fileModel.find({fid:fid})
+exports.ifHaveChild = function (fid) {
+  return fileModel.find({fid: fid})
 }
 
-exports.getFiles = function (cid,father) {
-  return fileModel.find({cid:cid,father:father})
+getAllFiles = async function (cid, fid) {
+  const res = await fileModel.find({cid: cid, fid: fid},{title:1,fid:1,childNodes:1})
+  let tree = res[0]
+  if (!tree && tree.childNodes.length === 0) {
+    return tree
+  }
+
+  tree["_doc"]["child"] = []
+  for(item of tree.childNodes){
+    tree["_doc"]["child"].push(await getAllFiles(cid, item.fid));
+  }
+  return tree
 }
 
-exports.getNextSequenceValue = function (sequenceName){
-  return countersModel.findOneAndUpdate({name:sequenceName},{$inc: {id: 1}},{new: true});
+exports.getAllFiles = getAllFiles
+
+exports.getNextSequenceValue = function (sequenceName) {
+  return countersModel.findOneAndUpdate({name: sequenceName}, {$inc: {id: 1}}, {new: true});
 }
